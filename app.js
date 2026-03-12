d77805194934deb48bef268309615514c8f024d7
Thepotato2 <the_spud_2021@protonmail.com>
2026-03-11 21:44:26 -0500

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello from the Raspberry Pi! also you stink\n');
+});
+
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://your-pi-ip:${port}/`);
+});
