9b8434128bcd22893cf7c721cfbe041a4647dfcd
Thepotato2 <the_spud_2021@protonmail.com>
2026-03-11 22:01:23 -0500

    updated server feedback for more accurate customer discriptor set

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+// ping thepotato.local
+// ssh thepotato@<IP adress>
+// cd my-server/raspi-nodeserver
+// node app.js
+
 const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from the Raspberry Pi! also you stink\n');
+  res.end('Hello from the Raspberry Pi! also you stink, and you look like a tard\n');
 });
 
 server.listen(port, '0.0.0.0', () => {
